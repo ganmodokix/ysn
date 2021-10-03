429867a9cb520c444f68894a75626564206c27fe
Ganmodokix <lugerp07@live.jp>
2021-10-03 13:51:09 +0900

    added flow legacies

---
+++ template/flow/mincostflow.cpp
@@ -0,0 +1,67 @@
+// 最小費用流 ポテンシャル付き O(FElogV)
+// CapacityとCostは符号付き整数型である必要がある（整数型でないと停止しない場合がある，符号付きでないと逆辺がバグる）
+// Verified by AOJ GRL_6_B http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_B
+template <typename Capacity = long long, typename Cost = long long>
+struct min_cost_flow {
+
+    using edge = tuple<size_t, Capacity, Cost, size_t>; // {to, cap, cost, rev}
+    
+    size_t n;
+    vector<vector<edge>> g;
+
+    min_cost_flow(): n(0), g() {}
+    min_cost_flow(size_t _n): n(_n), g(n) {}
+
+    void add_arc(size_t from, size_t to, Capacity cap, Cost cost) {
+        g[from].emplace_back(to  , cap,  cost, g[to].size());
+        g[to  ].emplace_back(from,   0, -cost, g[from].size()-1);
+    }
+
+    // 流せない場合，numeric_limits<Cost>::max()が返る
+    Cost min_cost(size_t s, size_t t, Capacity f) {
+        Cost CINF = numeric_limits<Cost>::max();
+        Cost FINF = numeric_limits<Capacity>::max();
+        Cost result = 0;
+        vector<Cost> h(n, 0); // ポテンシャル
+        while (f > 0) {
+            // Dijkstra法+ポテンシャルで最短路を見つける
+            using qitem = pair<Cost, size_t>;
+            priority_queue<qitem, vector<qitem>, greater<qitem>> q;
+            vector<Cost> dist(n, CINF);
+            vector<size_t> prevv(n), preve(n);
+            dist[s] = 0;
+            q.push(qitem(0, s));
+            while (q.size()) {
+                const auto du = q.top(); q.pop();
+                Cost d; size_t u; tie(d, u) = du;
+                REP(i, g[u].size()) {
+                    size_t to, rev; Capacity cap; Cost cost;
+                    tie(to, cap, cost, rev) = g[u][i];
+                    if (cap > 0 && chmin(dist[to], dist[u] + cost + h[u] - h[to])) {
+                        prevv[to] = u;
+                        preve[to] = i;
+                        q.push(qitem(dist[to], to));
+                    }
+                }
+            }
+            if (dist[t] == CINF) return CINF;
+            REP(i, n) h[i] += dist[i];
+            // 最短路に流す
+            Capacity d = f;
+            for (size_t i = t; i != s; i = prevv[i]) {
+                auto &e = g[prevv[i]][preve[i]];
+                chmin(d, get<1>(e));
+            }
+            f -= d;
+            result += d * h[t];
+            for (size_t i = t; i != s; i = prevv[i]) {
+                auto &e = g[prevv[i]][preve[i]];
+                auto &r = g[i][get<3>(e)];
+                get<1>(e) -= d;
+                get<1>(r) += d;
+            }
+        }
+        return result;
+    }
+
+};
---
+++ template/flow/mincostflow_nopotential.cpp
@@ -0,0 +1,63 @@
+// 最小費用流 ポテンシャルなし O(FVE)
+// CapacityとCostは符号付き整数型である必要がある（整数型でないと停止しない場合がある，符号付きでないと逆辺がバグる）
+// Verified by AOJ GRL_6_B http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_B
+template <typename Capacity = long long, typename Cost = long long>
+struct min_cost_flow {
+
+    using edge = tuple<size_t, Capacity, Cost, size_t>; // {to, cap, cost, rev}
+    
+    size_t n;
+    vector<vector<edge>> g;
+
+    min_cost_flow(): n(0), g() {}
+    min_cost_flow(size_t _n): n(_n), g(n) {}
+
+    void add_arc(size_t from, size_t to, Capacity cap, Cost cost) {
+        g[from].emplace_back(to  , cap,  cost, g[to].size());
+        g[to  ].emplace_back(from,   0, -cost, g[from].size()-1);
+    }
+
+    // 流せない場合，numeric_limits<Cost>::max()が返る
+    Cost min_cost(size_t s, size_t t, Capacity f) {
+        Cost CINF = numeric_limits<Cost>::max();
+        Cost FINF = numeric_limits<Capacity>::max();
+        Cost result = 0;
+        while (f > 0) {
+            // Bellman-Ford法で最短路を見つける
+            vector<Cost> dist(n, CINF);
+            vector<size_t> prevv(n), preve(n);
+            dist[s] = 0;
+            for (bool yet = true; yet; ) {
+                yet = false;
+                REP(i, n) if (dist[i] < CINF) {
+                    REP(j, g[i].size()) {
+                        size_t to, rev; Capacity cap; Cost cost;
+                        tie(to, cap, cost, rev) = g[i][j];
+                        if (cap > 0 && chmin(dist[to], dist[i] + cost)) {
+                            prevv[to] = i;
+                            preve[to] = j;
+                            yet = true;
+                        }
+                    }
+                }
+            }
+            if (dist[t] == CINF) return CINF;
+            // 最短路に流す
+            Capacity d = f;
+            for (size_t i = t; i != s; i = prevv[i]) {
+                auto &e = g[prevv[i]][preve[i]];
+                chmin(d, get<1>(e));
+            }
+            f -= d;
+            result += d * dist[t];
+            for (size_t i = t; i != s; i = prevv[i]) {
+                auto &e = g[prevv[i]][preve[i]];
+                auto &r = g[i][get<3>(e)];
+                get<1>(e) -= d;
+                get<1>(r) += d;
+            }
+        }
+        return result;
+    }
+
+};
