cefb1bceaeb2aabdd565f92651d2c45a48f5e879
Ganmodokix <lugerp07@live.jp>
2021-10-10 12:16:57 +0900

    added cumsum

---
+++ template/opf.cpp
@@ -0,0 +1,8 @@
+namespace opf {
+    struct min { template <typename T> T operator() (const T &x, const T &y) const { return std::min(x, y); } };
+    struct max { template <typename T> T operator() (const T &x, const T &y) const { return std::max(x, y); } };
+    struct gcd { template <typename T> T operator() (const T &x, const T &y) const { return std::gcd(x, y); } };
+    struct lcm { template <typename T> T operator() (const T &x, const T &y) const { return std::lcm(x, y); } };
+    struct plus { template <typename T> T operator() (const T &x, const T &y) const { return x + y; } };
+    struct multiply { template <typename T> T operator() (const T &x, const T &y) const { return x * y; } };
+};
