f0ee4fc84ae4ae290b3e87aee8b899b66f6384d5
Ganmodokix <lugerp07@live.jp>
2021-11-20 22:48:56 +0900

    fixed modint_petit

--- template/modint_petit.cpp
+++ template/modint_petit.cpp
@@ -1,10 +1,14 @@
 // 剰余演算ライブラリ (小): 剰余と逆元だけ欲しいときに使う軽量ライブラリ
 // mod pdiv 上での a^n (pdiv は素数である必要はない)
-ll modpow(const ll a, ll n, const ll pdiv) {
-    if (a == 0) return 0;
-    ll r = 1;
-    for (ll b = (a % pdiv + pdiv) % pdiv; n; n >>= 1, (b *= b) %= pdiv) if (n & 1) (r *= b) %= pdiv;
-    return r;
+ll modpow(ll a, ll n, const ll pdiv) {
+    a %= pdiv; if (a < 0) a += pdiv;
+    n %= pdiv-1; if (n < 0) n += pdiv-1;
+    long long result = 1;
+    for (long long base = a; n; n >>= 1) {
+        if (n & 1) (result *= base) %= pdiv;
+        (base *= base) %= pdiv;
+    }
+    return result;
 }
 // mod pdiv 上での逆元を求める (pdiv は素数である必要が**ある**, 素数じゃなかったらACLを使う)
 ll modinv(const ll a, const ll pdiv) { return modpow(a, pdiv-2, pdiv); }
