8ce44097af24bc7ea42ad7883f679b3a89946ee0
Ganmodokix <lugerp07@live.jp>
2022-09-24 17:04:57 +0900

    add tree

---
+++ template/graph/rooted_tree.cpp
@@ -0,0 +1,79 @@
+// #REQ: base_template.cpp graph.cpp graph/01bfs.cpp array/argsort.cpp
+// 連結の根付き木
+struct rooted_tree {
+
+    ll n;
+    ll root;
+    vector<pair<ll, ll>> parent; // {parent, cost to parent}
+    static const ll NO_PARENT = -1;
+    vector<vector<pair<ll, ll>>> children;
+    vector<ll> depth;
+
+    rooted_tree() = default;
+
+    // 無向グラフから構成
+    rooted_tree(ll root_, const graph &g):
+        n(g.size()), root(root_), parent(), children(n), depth()
+    {
+        const auto &[dist, from] = dist_01bfs(root, g);
+        parent = vector(n, pair(-1LL, -1LL));
+        REP(i, n) {
+            parent[i] = {from[i], from[i] == -1 ? 0 : dist[i] - dist[from[i]]};
+        }
+        build_children_from_parent();
+        depth = dist;
+    }
+
+    // par[u] から構成
+    template<typename T>
+    rooted_tree(ll root_, const vector<T> &par):
+        n(par.size()), root(root_), parent(n), children(n), depth()
+    {
+        REP(i, par.size()) {
+            parent[i] = par[i];
+        }
+        build_children_from_parent();
+        build_depth_from_parent();
+    }
+
+    // 構成用関数
+    void build_children_from_parent() {
+        children = vector(n, vector<pair<ll, ll>>());
+        REP(i, n) {
+            const auto &[par, cost] = parent[i];
+            if (par != -1) {
+                children[par].emplace_back(i, cost);
+            }
+        }
+    }
+    void build_depth_from_parent() {
+        depth = vector<ll>(n, LINF);
+        deque<ll> q = {root};
+        depth[root] = 0;
+        while (q.size()) {
+            const auto u = q.front(); q.pop_front();
+            for (const auto &[v, cost] : children[u]) {
+                if (chmin(depth[v], depth[u] + 1)) {
+                    q.push_back(v);
+                } else {
+                    DUMP("Not a tree");
+                    assert(false);
+                }
+            }
+        }
+    }
+
+    size_t size() const { return n; }
+
+    // top-down DP 用 argsort
+    vector<size_t> argsort_topdown() const {
+        return argsort(depth);
+    }
+    // bottom-up DP 用 argsort
+    vector<size_t> argsort_bottomup() const {
+        auto v = argsort(depth);
+        reverse(ALL(v));
+        return v;
+    }
+
+};
---
+++ template/graph/tree_doubling.cpp
@@ -0,0 +1,65 @@
+// #REQ: base_template.cpp graph.cpp/rooted_tree.cpp bisection.cpp
+// 根付き木上でのダブリング
+struct tree_doubling {
+    
+    ll n;
+    const rooted_tree &rt;
+    vector<vector<ll>> parent_func; // v
+    vector<vector<ll>> cost_func; // cost
+    ll maxdoubling;
+
+    // O(NlogN)
+    tree_doubling(const rooted_tree &rt_):
+        n(rt_.size()), rt(rt_), parent_func(), cost_func(), maxdoubling(0)
+    {
+        while ((1LL << maxdoubling) < n) ++maxdoubling;
+        parent_func = vector(maxdoubling + 1, vector(n, -1LL));
+        cost_func = vector(maxdoubling + 1, vector(n, 0LL));
+        REP(x, n) {
+            parent_func[0][x] = rt.parent[x].first;
+            cost_func[0][x] = rt.parent[x].second;
+            if (parent_func[0][x] == -1) {
+                parent_func[0][x] = x;
+            }
+        }
+        REP(i, maxdoubling) {
+            REP(x, n) {
+                parent_func[i+1][x] = parent_func[i][parent_func[i][x]];
+                cost_func[i+1][x] = cost_func[i][x] + cost_func[i][parent_func[i][x]];
+            }
+        }
+    }
+
+    // {uからn個だけ登った頂点, u-v間のコスト} O(log n)
+    pair<ll, ll> nth_parent(ll u, ll n) const {
+        ll v = u;
+        ll cost = 0;
+        for (ll i = 0, m = n; m; i++, m >>= 1) {
+            if (m & 1) {
+                cost += cost_func[i][v];
+                v = parent_func[i][v];
+            }
+        }
+        return {v, cost};
+    }
+
+    // {LCA, u-l間のコスト, v-l間のコスト} O(log #V)
+    tuple<ll, ll, ll> lca(ll u, ll v) const {
+        if (rt.depth[u] > rt.depth[v]) {
+            const auto &[l, vbc, ubc] = lca(v, u);
+            return {l, ubc, vbc};
+        }
+        ll ubc = 0, vbc = 0, vbcc = 0, l;
+        tie(v, vbcc) = nth_parent(v, rt.depth[v] - rt.depth[u]);
+        ll d = bisection<ll>(-1, rt.depth[u], [&](ll m) { return nth_parent(u, m) == nth_parent(v, m); });
+        tie(l, ubc) = nth_parent(u, d);
+        tie(std::ignore, vbc) = nth_parent(v, d);
+        return {l, ubc, vbc + vbcc};
+    }
+
+    ll dist(ll u, ll v) const {
+        auto [l, ubc, vbc] = lca(u, v);
+        return ubc + vbc;
+    }
+
+};
