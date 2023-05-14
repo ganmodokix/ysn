e1b54f78e38f8aeb68990c11b17eef9768f563f7
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2023-05-14 19:02:05 +0900

    Fix type

--- template/number/ceillog2.cpp
+++ template/number/ceillog2.cpp
@@ -4,7 +4,7 @@
 template <typename T, typename enable_if<is_integral_v<T>, nullptr_t>::type = nullptr>
 T ceillog2(T n) {
     assert(n <= numeric_limits<T>::max() / 2);
-    ll h = 1, r = 0;
+    T h = 1, r = 0;
     while (h < n) {
         h <<= 1;
         r++;
