9d27f1cb27921b7827a5b24d2124d1db52ac86de
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2023-06-07 11:39:48 +0900

    Add fast hadamard transform

---
+++ template/conv/hadamard.cpp
@@ -0,0 +1,21 @@
+// #REQ: base_template number/ceillog2.cpp
+
+// 高速 Walsh-Hadamard 変換 O(N log N)
+template <typename T>
+vector<T> fht(vector<T> a, const bool inv = false) {
+    assert(popcountll(a.size()) == 1);
+    const auto n = static_cast<ll>(a.size());
+    for (auto i = 1; i < n; i <<= 1) {
+        for (auto j = n; (--j &= ~i) >= 0; ) {
+            const auto u = a[j];
+            const auto v = a[j + i];
+            a[j  ] = u + v;
+            a[j + i] = u - v;
+            if (inv) {
+                a[j] /= 2;
+                a[j + i] /= 2;
+            }
+        }
+    }
+    return a;
+}
