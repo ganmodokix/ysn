29dc0fb812d07bc8d53293d0838c995e729517d6
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2023-06-18 16:04:57 +0900

    Fix sahz

--- template/array/sahz.cpp
+++ template/array/sahz.cpp
@@ -1,13 +1,12 @@
 // #REQ: base_template
 // 座圧 {写像, 逆写像} が返る O(NlogN)
-template <typename T>
-pair<map<T, int>, vector<T>> sahz(const vector<T> &x0) {
-    vector<T> x = x0;
-    sort(x.begin(), x.end());
-    x.erase(unique(x.begin(), x.end()), x.end());
-    map<T, int> r;
-    for (int i = 0; i < (int)x.size(); i++) {
+// verified by https://atcoder.jp/contests/abc036/tasks/abc036_c
+template <typename T, typename U = size_t, typename M = unordered_map<T, U>>
+pair<M, vector<T>> sahz(vector<T> x) {
+    UNIQUE(x);
+    M r;
+    REP(i, x.size()) {
         r[x[i]] = i;
     }
-    return make_pair(r, x);
+    return {r, x};
 }
