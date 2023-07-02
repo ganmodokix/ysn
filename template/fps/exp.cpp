567f251ff1da27578bdf27d60c8745f4c9ff4891
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2023-07-02 22:42:38 +0900

    Add FPS operations

---
+++ template/fps/exp.cpp
@@ -0,0 +1,28 @@
+// #REQ: base_template.cpp fps/log.cpp conv/ntt.cpp
+
+// FPS exp
+template <ll pdiv, ll prim>
+vector<ll> fps_exp(const vector<ll>& f) {
+
+    assert(f[0] % pdiv == 0);
+    auto g = vector<ll>{1};
+
+    while (g.size() < f.size()) {
+        const auto ngg = g.size() * 2;
+
+        // 1 - log g + f
+        auto gg = g;
+        gg.resize(ngg, 0);
+        gg = fps_log<pdiv, prim>(move(gg));
+        REP(i, ngg) {
+            gg[i] = (!i + pdiv - gg[i] + (i < (ll)f.size() ? f[i] : 0)) % pdiv;
+        }
+
+        // g (1 - log g + f)
+        g = convolve_p<pdiv, prim>(move(g), gg);
+        g.resize(ngg, 0);
+    }
+    
+    g.resize(f.size(), 0);
+    return g;
+}
