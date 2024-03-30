602fd0e918017759b107f25667a94f9cc754ac22
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2024-03-30 14:09:42 +0900

    🔥 Replace popcount macro with C++20 std::popcount

--- template/base_template.hpp
+++ template/base_template.hpp
@@ -2,7 +2,6 @@
 #include "base_template/575.hpp"
 #include "base_template/ll.hpp"
 #include "base_template/debug_mode.hpp"
-#include "base_template/gcc.hpp"
 #include "base_template/macro.hpp"
 #include "base_template/dump.hpp"
 #include "base_template/qin.hpp"
