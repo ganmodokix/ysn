48ed56dc1f3f3d5b0a60418eaec04456a8e5f2ee
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2025-03-24 21:07:32 +0900

    ✨ Use euler's phi for primitive roots of non-prime modulo

--- template/primitive_root.hpp
+++ template/primitive_root.hpp
@@ -1,28 +1,37 @@
 #pragma once
 #include "base_template.hpp"
 #include "modint_petit.hpp"
+#include "number/euler_phi.hpp"
 #include "marathon/xor128_generator.hpp"
 
+namespace primitive_root_detail {
+    template<integral T>
+    consteval pair<T, vector<pair<T, T>>> phi_prime_factors(const T n) {
+        assert(n >= 2);
+        auto pf = vector<pair<T, T>>{};
+        const auto phi = euler_phi(n);
+        {
+            auto n = phi;
+            for (auto i = T{2}; i <= n / i; ++i) {
+                if (n % i == 0) {
+                    pf.emplace_back(i, 1);
+                    do { n /= i; pf.back().second *= i; } while (n % i == 0);
+                }
+            }
+            if (n > 1) {
+                pf.emplace_back(n, n);
+            }
+        }
+        return {phi, move(pf)};
+    }
+}
+
 // 乱択で原始根を求める
-// UB if pdiv is not a prime
 consteval ll random_primitive_root(const ll pdiv) {
-    assert(pdiv == 2 || (pdiv >= 2 && pdiv % 2 == 1));
+    assert(pdiv >= 2);
 
     // 素因数分解
-    auto pf = vector<pair<ll, ll>>{};
-    auto n = pdiv - 1;
-    for (auto i = 2LL; i * i <= n; ++i) {
-        while (n % i == 0) {
-            n /= i;
-            if (pf.empty() || pf.back().first != i) {
-                pf.emplace_back(i, 1);
-            }
-            pf.back().second *= i;
-        }
-    }
-    if (n > 1) {
-        pf.emplace_back(n, n);
-    }
+    const auto [phi, pf] = primitive_root_detail::phi_prime_factors(pdiv);
 
     // 乱択
     auto rd = xor128_generator(pdiv);
@@ -31,11 +40,11 @@ consteval ll random_primitive_root(const ll pdiv) {
         auto a = -1LL;
         while (true) {
             a = rd() % pdiv;
-            if (modpow(a, (pdiv - 1) / q, pdiv) != 1) {
+            if (modpow(a, phi / q, pdiv) != 1) {
                 break;
             }
         }
-        (g *= modpow(a, (pdiv - 1) / qe, pdiv)) %= pdiv;
+        (g *= modpow(a, phi / qe, pdiv)) %= pdiv;
     }
     return g;
 }
@@ -43,26 +52,16 @@ consteval ll random_primitive_root(const ll pdiv) {
 // 最小の原始根を総当たりで求める pdiv が素数であればすぐ終わるはず
 // コンパイル時に行ってしまうことを推奨
 consteval ll primitive_root(const ll pdiv) {
-    assert(pdiv == 2 || (pdiv >= 2 && pdiv % 2 == 1));
+    assert(pdiv >= 2);
 
     // 素因数分解
-    auto pf = vector<ll>{};
-    auto n = pdiv - 1;
-    for (auto i = 2LL; i * i <= n; ++i) {
-        if (n % i == 0) {
-            pf.emplace_back(i);
-            do { n /= i; } while (n % i == 0);
-        }
-    }
-    if (n > 1) {
-        pf.emplace_back(n);
-    }
+    const auto [phi, pf] = primitive_root_detail::phi_prime_factors(pdiv);
 
     // 線形探索
-    for (auto i = 2; i < pdiv; ++i) {
+    for (auto i = 2LL; i < pdiv; ++i) {
         auto okay = true;
-        for (const auto f : pf) {
-            if (modpow(i, (pdiv - 1) / f, pdiv) == 1) {
+        for (const auto& [f, _] : pf) {
+            if (modpow(i, phi / f, pdiv) == 1) {
                 okay = false;
                 break;
             }
