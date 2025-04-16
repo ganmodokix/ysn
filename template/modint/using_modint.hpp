b0ae2605772ce6330ba80afef1895c007907f294
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2025-04-16 09:36:01 +0900

    🎨 Use macro for using modint as fixed mod

---
+++ template/modint/using_modint.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "base_template.hpp"
+#include "modint/modint.hpp"
+#include "modint/modint_factorial_cache.hpp"
+
+// moduloint<PDIV> を modint 型として定義する
+// 2回以上呼び出すとコンパイルエラーになるはず
+#define USING_MODINT(PDIV) \
+    using modint = moduloint<(PDIV)>; \
+    \
+    /* IO */ \
+    constexpr modint operator"" _p(unsigned long long _x) noexcept { return modint(_x); } \
+    ostream& operator<< (ostream& ost, const modint a) { return ost << a.item(); } \
+    istream& operator>> (istream& ist, modint &a) { long long x = {}; ist >> x; a = modint(x); return ist; } \
+    \
+    /* Factorial n! */ \
+    factorial_cache<modint> fac_cache; \
+    modint    fac(long long n) { fac_cache.prepare(n); return fac_cache.fac[n]; } \
+    modint invfac(long long n) { fac_cache.prepare(n); return fac_cache.invfac[n]; } \
+    constexpr bool isinpascal(long long n, long long r) noexcept { return 0 <= r && r <= n; } \
+    modint nCr(long long n, long long r) { return !isinpascal(n, r) ? 0_p : fac(n) * invfac(r) * invfac(n-r); } \
+    modint nPr(long long n, long long r) { return !isinpascal(n, r) ? 0_p : fac(n) * invfac(n-r); } \
+    modint nHr(long long n, long long r) { return nCr(n+r-1, r); } \
+    modint catalan(long long n) { return nCr(n * 2, n) / (n + 1); }
