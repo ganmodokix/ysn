f290c4d5ec2569b791f951436feb8c4d17ad90dc
ganmodokix <1450602+ganmodokix@users.noreply.github.com>
2026-02-11 16:38:44 +0900

    ✨ Add projection support to sahz

--- template/array/sahz.hpp
+++ template/array/sahz.hpp
@@ -1,13 +1,37 @@
 #pragma once
 #include "base_template.hpp"
-// 座圧 {写像, 逆写像} が返る O(NlogN)
-// verified by https://atcoder.jp/contests/abc036/tasks/abc036_c
-template <typename T, typename U = size_t, typename M = unordered_map<T, U>>
-pair<M, vector<T>> sahz(vector<T> x) {
-    UNIQUE(x);
-    M r;
-    REP(i, x.size()) {
-        r[x[i]] = i;
+#include "ranges_to.hpp"
+
+// 座圧 {写像, 逆写像} が返る
+// Map が std::unordered_map の場合、平均 O(N) 最悪 O(N^2)
+// Map が std::map の場合、平均 O(NlogN) 最悪 O(NlogN)
+// verified at <<WIP>>
+template <
+    typename R,
+    typename Map = unordered_map<ranges::range_value_t<R>, ptrdiff_t>,
+    typename Comp = less<>,
+    typename Proj = identity
+>
+auto sahz(R&& r, Comp comp = {}, Proj proj = {})
+    -> pair<Map, vector<ranges::range_value_t<R>>>
+{
+    using T = ranges::range_value_t<R>;
+    auto unique_values = r | to_vector<T>{};
+    {
+        ranges::sort(unique_values, comp, proj);
+        auto equiv_comp = [&](const auto& x, const auto& y) -> bool {
+            return !comp(x, y) && !comp(y, x);
+        };
+        const auto erased_range = ranges::unique(unique_values, equiv_comp, proj);
+        unique_values.erase(erased_range.begin(), erased_range.end());
     }
-    return {r, x};
+
+    auto smap = Map{};
+    auto index = ptrdiff_t{0};
+    for (auto&& x : unique_values) {
+        smap[x] = index++;
+    }
+
+    return pair{move(smap), move(unique_values)};
+
 }
